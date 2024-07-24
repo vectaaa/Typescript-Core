415343462e332bea2fa40e1f8f26ea276d4090e9
Vecta <victoronuchukwu16@gmail.com>
2024-07-24 16:33:15 +0100

    Typescript Tuples

---
+++ myTupEnums.ts
@@ -0,0 +1,24 @@
+let tUser: [string, number, boolean];
+
+tUser = ["reader", 12, false];
+
+let rgb: [number, number, number] = [255, 123, 112];
+
+
+//Tuples types
+type User = [number, string]
+
+const newUser: User =  [112, "vecta@yahoo.com"]
+
+newUser[1] = "hsd@gmail.com"
+// newUser.push(true)
+
+
+
+
+
+
+
+
+
+export {}
